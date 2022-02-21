d15099bc4c007eb8edd8185d2ff57b4887e8f17b
S. Espinet <s.lou.espinet@gmail.com>
2022-02-20 19:18:07 -0800

    add properties

--- Models/Car.cs
+++ Models/Car.cs
@@ -3,10 +3,11 @@ namespace Dealership.Models
 
   public class Car
   {
-    private string _makeModel;
-    private int _price;
-    private int _miles;
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
 
+    
     public static string MakeSound(string sound)
     {
       return "Our cars sound like " + sound;
@@ -19,25 +20,25 @@ namespace Dealership.Models
 
     public Car(string makeModel, int price, int miles)
     {
-      _makeModel = makeModel;
-      _price = price;
-      _miles = miles;
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
     }
 
-    public string GetMakeModel()
-    {
-      return _makeModel;
-    }
+    // public string GetMakeModel()
+    // {
+    //   return _makeModel;
+    // }
 
-    public int GetPrice()
-    {
-      return _price;
-    }
+    // public int GetPrice()
+    // {
+    //   return _price;
+    // }
 
-    public int GetMiles()
-    {
-      return _miles;
-    }
+    // public int GetMiles()
+    // {
+    //   return _miles;
+    // }
 
     public bool WorthBuying(int maxPrice)
     {
